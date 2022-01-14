6781d53011b59d3e9670a709e85f66d0014af59d
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-01-14 15:14:56 +0100

    Update: provider and module
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  # required_providers {
-  #   google = {
-  #     source  = "hashicorp/google"
-  #     version = ">= 4.0.0"
-  #   }
-  #   google-beta = {
-  #     source = "hashicorp/google"
-  #     version = ">= 4.0.0"
-  #   }
-  # }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+    #   google-beta = {
+    #     source  = "hashicorp/google"
+    #     version = "4.5.0"
+    #   }
+  }
 }
